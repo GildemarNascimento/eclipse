8bd39da8460d8730f58a666e179c1f4226f754c2
GildemarNascimento <gildemargg@hotmail.com>
2020-10-30 13:32:49 -0300

    Commit inicial

---
+++ HelloGitHub/src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// Exemplo de integração do Eclipse com Git
+		
+		System.out.println("Hello Git");
+		
+
+	}
+
+}
